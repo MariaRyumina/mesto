4ba55b4182ea90ef470153e691b198a5611dde7b
Maria Ryumina <maria.ryumin@gmail.com>
2023-05-06 15:47:28 +1000

    feat: завершить верстку странички сайта

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,28 @@
 let buttonOpenPopup = document.querySelector('.profile__button-edit');
-let popup = document.querySelector('.popup');
 let buttonClosePopup = document.querySelector('.popup__close');
-let buttonSavePopup = document.querySelector('.popup__button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_value_name');
+let aboutInput = document.querySelector('.popup__input_value_about');
+let profileName = document.querySelector('.profile__title');
+let profileAbout = document.querySelector('.profile__subtitle');
 
 buttonOpenPopup.addEventListener('click', function  () {
     popup.classList.add('popup__opened');
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
 });
 
 buttonClosePopup.addEventListener('click', function  () {
     popup.classList.remove('popup__opened');
 });
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    // Вставьте новые значения с помощью textContent
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    popup.classList.remove('popup__opened');
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
